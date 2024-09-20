1c4cdb2ceb786cc729162e4e2bac837614544b28
Etienne Vaillant <vaillant.etienne@gmail.com>
2024-09-20 21:33:30 +0200

    PortfolioPerformance / Add export instrument

--- src/output/portfolio_performance.rs
+++ src/output/portfolio_performance.rs
@@ -2,10 +2,13 @@ use super::Output;
 use crate::error::Error;
 use crate::portfolio::{CashVariationSource, Portfolio, Way};
 use log::debug;
+use std::collections::HashSet;
 
 use std::fs::File;
 use std::io::Write;
 
+//
+// doc https://help.portfolio-performance.info/en/reference/file/import/csv-import/
 pub struct PortfolioPerformanceOutput<'a> {
     output_dir: String,
     portfolio: &'a Portfolio,
@@ -61,10 +64,41 @@ impl<'a> PortfolioPerformanceOutput<'a> {
         }
         Ok(())
     }
+
+    fn write_instrument(&self) -> Result<(), Error> {
+        let filename = format!("{}/{}_instrument.csv", self.output_dir, self.portfolio.name);
+        let mut output_stream = File::create(filename)?;
+        output_stream.write_all("Ticker Symbol;ISIN;Security Name;Currency\n".as_bytes())?;
+        for instrument in self
+            .portfolio
+            .positions
+            .iter()
+            .map(|position| &position.instrument)
+            .collect::<HashSet<_>>()
+        {
+            let mut buffer = String::new();
+            if instrument.ticker_yahoo.is_some() {
+                buffer.push_str(instrument.ticker_yahoo.as_ref().unwrap().as_str());
+            }
+            buffer.push(';');
+            buffer.push_str(instrument.isin.as_str());
+            buffer.push(';');
+            buffer.push_str(instrument.description.as_str());
+            buffer.push(';');
+            buffer.push_str(instrument.currency.name.as_str());
+            buffer.push('\n');
+            output_stream.write_all(buffer.as_bytes())?;
+        }
+
+        Ok(())
+    }
 }
 
 impl<'a> Output for PortfolioPerformanceOutput<'a> {
     fn write(&mut self) -> Result<(), Error> {
+        debug!("write instrument");
+        self.write_instrument()?;
+
         debug!("write account");
         self.write_account()?;
 
